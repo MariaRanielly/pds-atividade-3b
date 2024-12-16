06e1997dad048172a146e197389454de376ce2d4
Amanda <amandamendes0205@gmail.com>
2024-12-16 17:00:23 -0300

    Projeto pronto

---
+++ Atividade/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+		
+	    public static void main(String[] args) {
+	        
+	        Pessoa pessoa1 = new Pessoa("Maria Ranielly", 20);
+	        Pessoa pessoa2 = new Pessoa("Amanda", 20);
+	        Pessoa pessoa3 = new Pessoa("Luciendson", 20);
+	        
+	        
+	        pessoa1.exibirInformacoes();
+	        pessoa2.exibirInformacoes();
+	        pessoa3.exibirInformacoes();
+	    }
+	}
+
